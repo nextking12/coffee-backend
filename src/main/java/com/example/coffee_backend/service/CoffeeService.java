39694b09c14498e314ab1bb33530616ed48bf98b
Edward King <king8three@gmailcom>
2025-09-05 13:44:26 -0400

    working on service

--- src/main/java/com/example/coffee_backend/service/CoffeeService.java
+++ src/main/java/com/example/coffee_backend/service/CoffeeService.java
@@ -1,4 +1,40 @@
 package com.example.coffee_backend.service;
 
+import com.example.coffee_backend.entity.CoffeeEntity;
+import com.example.coffee_backend.repository.CoffeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class CoffeeService {
+
+    private final CoffeeRepository coffeeRepository;
+
+
+    public CoffeeService(CoffeeRepository coffeeRepository) {
+        this.coffeeRepository = coffeeRepository;
+    }
+
+    public List<CoffeeEntity> findAll() {
+        return coffeeRepository.findAll();
+    }
+
+    public List<CoffeeEntity> findAllByOrigin(String origin) {
+        return coffeeRepository.findAllByOrigin(origin);
+    }
+
+    public List<CoffeeEntity> findAllByType(String type) {
+        return coffeeRepository.findAllByType(type);
+    }
+
+    public Optional<CoffeeEntity>findById(Long id) {
+        return coffeeRepository.findById(id);
+    }
+
+    public Optional<CoffeeEntity> findByName(String name) {
+        return coffeeRepository.findByName(name);
+    }
 }
