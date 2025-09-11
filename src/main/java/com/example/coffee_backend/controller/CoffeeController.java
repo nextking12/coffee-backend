56cdd061058800b63c276d78ecc8f4999e6056fb
Edward King <king8three@gmailcom>
2025-09-11 14:51:25 -0400

    added controller

--- src/main/java/com/example/coffee_backend/controller/CoffeeController.java
+++ src/main/java/com/example/coffee_backend/controller/CoffeeController.java
@@ -1,4 +1,34 @@
 package com.example.coffee_backend.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.coffee_backend.entity.CoffeeEntity;
+import com.example.coffee_backend.service.CoffeeService;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+@RequestMapping("/api/v1")
 public class CoffeeController {
+
+    private final CoffeeService coffeeService;
+
+    public CoffeeController(CoffeeService coffeeService){
+        this.coffeeService = coffeeService;
+    }
+
+
+
+    @PostMapping("/coffees")
+    public ResponseEntity<CoffeeEntity>createCoffeeEntry(@RequestBody CoffeeEntity coffeeEntity) {
+        CoffeeEntity createdCoffeeEntity = coffeeService.createCoffee(coffeeEntity);
+        return new ResponseEntity<>(createdCoffeeEntity, HttpStatus.CREATED);
+        
+    }
+    
 }
