eeddd4ad55d2fb62fd0f4728000ef5169c7653c1
Edward King <king8three@gmail.com>
2025-09-20 22:16:05 -0400

    added auth controller

---
+++ src/main/java/com/example/coffee_backend/controller/AuthController.java
@@ -0,0 +1,13 @@
+package com.example.coffee_backend.controller;
+
+import com.example.coffee_backend.repository.UserRepository;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/coffee/auth")
+public class AuthController {
+
+}
--- src/main/java/com/example/coffee_backend/repository/UserRepository.java
+++ src/main/java/com/example/coffee_backend/repository/UserRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.coffee_backend.entity.User;
 
-interface UserRepository extends JpaRepository<User,Long> {
+public interface UserRepository extends JpaRepository<User,Long> {
     Optional<User>findByUsername(String username);
     Optional<User>findByEmail(String email);
     boolean existsByUsername(String username);
